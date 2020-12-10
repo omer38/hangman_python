f1d373e18d1e72b6c14adc979f98880a68577e78
omer38 <69686830+omer38@users.noreply.github.com>
2020-12-11 01:24:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import random
+import aciiarts
+import hangman_words
+
+
+word_list=hangman_words.word_list
+
+print(aciiarts.logo)
+
+hangman_word = random.choice(word_list)
+list(hangman_word)
+display=[]
+for i in range(1,len(hangman_word)+1):
+    display+="_"
+print(f"hangi kelime görmek için {hangman_word}")
+
+end_of_game=False
+
+lives=6
+while not end_of_game:
+    user_input=input("Guess a letter: ").lower()
+
+    #displaydaki "_"ile girilen harfin yer değiştirmesi
+    for position in range(len(hangman_word)):
+        letter=hangman_word[position]
+        if letter==user_input:
+            display[position]=letter
+
+
+    if user_input not in hangman_word:
+        lives-=1
+        print(aciiarts.stages[lives])
+        if lives==0:
+            print("you lose")
+            end_of_game=True
+
+
+    if "_" not in display:
+        end_of_game=True
+        print("you won")
+
+    print(display)
+
+
+
